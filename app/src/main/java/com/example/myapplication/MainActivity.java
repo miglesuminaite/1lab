81b50c4485f8f3dc909951a4b221e3ddff9c4ba1
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-10-10 21:27:23 +0300

    Papildymas programiniu kodu, jog paspaudus button, TextView atsiranda uzrasas "Welcome to android"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+    }
+
+    public void button2(View view) {
+        TextView tv = (TextView)findViewById(R.id.tvChangeText);
+        tv.setText("Welcome to android");
     }
 }
