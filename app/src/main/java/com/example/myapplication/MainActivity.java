24509a1d357c8ad73b990a1b5fe9d036eeb01f82
miglesuminaite <159789395+miglesuminaite@users.noreply.github.com>
2024-10-10 21:59:52 +0300

    Papildymas programiniu kodu, kai paspaudus button3, pasikeicia teksto spalva

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.ColorKt;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -29,5 +31,10 @@ public class MainActivity extends AppCompatActivity {
     public void button2(View view) {
         TextView tv = (TextView)findViewById(R.id.tvChangeText);
         tv.setText("Welcome to android");
+    }
+
+    public void button3(View view) {
+        TextView tv = (TextView)findViewById(R.id.tvChangeText);
+        tv.setTextColor(Color.RED);
     }
 }
